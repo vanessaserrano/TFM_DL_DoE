51933222834953870b7996f2c029d67309bd8892
Serrano Molinero <vanessa.serrano@IQS.LOCAL>
2020-12-04 20:50:49 +0100

    DL last changes test code

--- TFM_DLG_pruebas.R
+++ TFM_DLG_pruebas.R
@@ -1,7 +1,7 @@
 # TFM Daniela Lama - DoE
-#HOLA HOLA 
+
 #variable con lista de paquetes deseados
-paqs<-c("tidyverse","shiny")
+paqs<-c("tidyverse","shiny","DT","rhandsontable")
 
 #variable de paquetes a instalar: son los deseados que NO est?n en los resultados de la columna 1 de la libreria. Si no especificaramos [,1], aparecer?a m?s informaci?n de los paquetes con library()$results. 
 paqs2Install<-paqs[!(paqs%in%library()$results[,1])]
@@ -56,28 +56,50 @@ app_ui<-fluidPage(
   #Tipo de plantilla 
   sidebarLayout(
     sidebarPanel(
-      width=5,
+      width=3,
       titlePanel(h3("Seleccione el caso de preferencia")),
       verticalLayout(
-          fileInput(inputId="cargar_archivo",label="Cargar caso"),
-          radioButtons(inputId="caso_x",label="Empezar un caso nuevo",choices=c("Caso libre","Caso propuesto","Caso simulado")),
-          wellPanel(style= "background: lightgray",
-                    textOutput("descrip_caso")
-                    ),
-          wellPanel(style= "background: lightblue",
-                    actionButton(inputId="intdatos",label="Empezar"),
-                    actionButton(inputId="ayuda",label="Ayuda")
-                    )
+        fileInput(inputId="cargar_archivo",label="Cargar caso"),
+        radioButtons(inputId="caso_x",label="Empezar un caso nuevo",choices=c("Caso libre","Caso propuesto","Caso simulado")),
+        wellPanel(style= "background: lightblue",
+                  textOutput("descrip_caso")
+        ),
+        wellPanel(
+          tags$head(
+            tags$style(HTML('#intdatos{background-color:lightblue}'))
+          ),
+          div(style="display:inline-block;width:32%;text-align: left;",
+              actionButton(inputId="ayuda",label="Ayuda")),
+          div(style="display:inline-block;width:32%;text-align: center;",
+              actionButton(inputId="autor",label="Autor")),
+          div(style="display:inline-block;width:32%;text-align: right;",
+              actionButton(inputId="intdatos",label="Empezar"))
         )
+      )
     ),
     mainPanel(
-      width=7,
+      width=9,
       #plotOutput("distPlot"), #si quisiera tener el plot por fuera
       #h es el tamaño de fuente
       tabsetPanel(id="tabs1",type="tabs",
-                  tabPanel(title="Guía",wellPanel(h4("Descripci?n de cada caso"))),
-                  tabPanel(title="Datos",flowLayout(tableOutput("data")),actionButton(inputId="irmatriz",label="Generar matriz")),
-                  tabPanel(title="Matriz",plotOutput("distPlot")),
+                  tabPanel(title="Guía",wellPanel(h4(""))),
+                  tabPanel(title="Datos",
+                           verticalLayout(
+                             splitLayout(
+                               numericInput(inputId="num_factores",label="Número de factores",value=0,min=1,step=1,max=20),
+                               actionButton(inputId="borrar_datos",label="Reset")),
+                             splitLayout(
+                               DT::dataTableOutput("tabla_factores",width=250),
+                               DT::dataTableOutput("tabla_niveles",width=250)),
+                             numericInput(inputId="num_interac",label="Número de interacciones",value=0,min=0,step=1),
+                             DT::dataTableOutput("tabla_interacciones",width=250),
+                             splitLayout(
+                               actionButton(inputId="calcmatriz",label="Generar diseño"),
+                               textOutput("diseño_sugerido")),
+                             tags$style(HTML('#irmatriz{background-color:lightblue}')),
+                             div(style="display:inline-block;width:32%;text-align: right;"),
+                             actionButton(inputId="irmatriz",label="Continuar"))),
+                  tabPanel(title="Matriz",actionButton(inputId="iranalisis",label="Analizar")),
                   tabPanel(title="Análisis"))
     )
   )
@@ -90,15 +112,136 @@ app_server<-function(input,output,session){
   observeEvent(input$intdatos,{
     updateTabsetPanel(session,"tabs1",selected="Datos")
   })
+  
   observeEvent(input$irmatriz,{
     updateTabsetPanel(session,"tabs1",selected="Matriz")
   })
   
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$obs), col = 'darkgray', border = 'white')})
+  observeEvent(input$iranalisis,{
+    updateTabsetPanel(session,"tabs1",selected="Análisis")
+  })
   
-  output$data<-renderTable(head(iris))
+  observeEvent(input$ayuda,{
+    showModal(modalDialog(
+      title="AYUDA",renderText("Aquí no sé cómo hacer espacios entre líneas")
+    ))
+  })
   
+  observeEvent(input$autor,{
+    showModal(modalDialog(
+      title="Información del autor",renderText("Misma historia que en ayuda")
+    ))
+  })
+  
+  tabla_dom=reactive({
+    nombre_columna_fac<-c("Nombre Factor","Nivel")
+    # d1=as.data.frame(matrix(nrow=input$num_factores,ncol=3))
+    d1=data.frame(matrix(nrow=input$num_factores,ncol=2))
+    if(input$num_factores==1){
+      row.names(d1)<-c("A")
+    }
+    if(input$num_factores==2){
+      row.names(d1)<-c("A","B")
+    }
+    if(input$num_factores==3){
+      row.names(d1)<-c("A","B","C")
+    }
+    if(input$num_factores==4){
+      row.names(d1)<-c("A","B","C","D")
+    }
+    if(input$num_factores==5){
+      row.names(d1)<-c("A","B","C","D","E")
+    }
+    if(input$num_factores==6){
+      row.names(d1)<-c("A","B","C","D","E","F")
+    }
+    if(input$num_factores==7){
+      row.names(d1)<-c("A","B","C","D","E","F","G")
+    }
+    if(input$num_factores==8){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H")
+    }
+    if(input$num_factores==9){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I")
+    }
+    if(input$num_factores==10){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J")
+    }
+    if(input$num_factores==11){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K")
+    }
+    if(input$num_factores==12){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L")
+    }
+    if(input$num_factores==13){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M")
+    }
+    if(input$num_factores==14){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N")
+    }
+    if(input$num_factores==15){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O")
+    }
+    if(input$num_factores==16){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P")
+    }
+    if(input$num_factores==17){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q")
+    }
+    if(input$num_factores==18){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R")
+    }
+    if(input$num_factores==19){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S")
+    }
+    if(input$num_factores==20){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T")
+    }
+    if(input$num_factores==21){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U")
+    }
+    if(input$num_factores==22){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V")
+    }
+    if(input$num_factores==23){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W")
+    }
+    if(input$num_factores==24){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X")
+    }
+    if(input$num_factores==25){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y")
+    }
+    if(input$num_factores==26){
+      row.names(d1)<-c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+    }
+    colnames(d1)<-nombre_columna_fac
+    d1=as.data.frame(d1)
+  })
+  
+  output$tabla_factores<-renderDataTable(tabla_dom(),editable=TRUE)
+  proxy=dataTableProxy("tabla_factores")
+  replaceData(proxy,"tabla_factores")
+  
+  tabla_inter=reactive({
+    nombre_columna_inter<-c("Factor 1","Factor 2")
+    d2=as.data.frame(matrix(nrow=input$num_interac,ncol=2))
+    colnames(d2)<-nombre_columna_inter
+    d2<-as.data.frame(d2)
+  })
+  
+  output$tabla_interacciones<-renderDataTable(tabla_inter(),editable=TRUE)
+  
+  tabla_niv=reactive({
+    
+    if(tabla_factores[,2]==2){
+      nombre_columna_niv<-c("Nivel 1","Nivel 2")
+      d3=data.frame(matrix(nrow=input$num_factores,ncol=2))
+      colnames(d3)<-nombre_columna_niv
+      d3<-as.data.frame(d3)}
+  })
+  
+  output$tabla_niveles<-renderDataTable(tabla_niv(),editable=TRUE)
   
   # tipo_caso<-reactiveValues(libre=0,paper=0,simul=0)
   # 
@@ -119,6 +262,15 @@ app_server<-function(input,output,session){
   #   tipo_caso$paper<-0
   #   tipo_caso$simul<-1
   # })
+  
+  observeEvent(input$borrar_datos,{
+    updateNumericInput(session,"num_factores",value=0)
+    updateNumericInput(session,"num_interac",value=0)
+  })
+  
+  observeEvent(input$calcmatriz,{
+    output$diseno_sugerido<-renderText("Diseño sugerido, debe depender de alguna variable de calc")
+  })
   
   observeEvent(input$caso_x,{
     output$descrip_caso<-renderText({
